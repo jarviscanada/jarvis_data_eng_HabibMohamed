1493336ff4661bb5b98933a2b0f8fe463ebc8343
Habib Mohamed <abdirahiimm@gmail.com>
2021-01-12 15:53:07 +0000

    Implementation of host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -36,7 +36,5 @@ insert_query="INSERT INTO host_usage
 		info.hostname = '$hostname'
 	"
 
-echo "$insert_query"
-
 #Run insert query
 psql -h $psql_host -U $psql_user -d $db_name -p $psql_port -c "$insert_query"
