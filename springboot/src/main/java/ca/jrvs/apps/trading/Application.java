cff18bd3779e310e2b2344e1810e616ccedd28f5
Habib Mohamed <abdirahiimm@gmail.com>
2021-02-09 11:23:00 -0500

    Updating Application
    Leaving it blank for now

--- springboot/src/main/java/ca/jrvs/apps/trading/Application.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -1,10 +1,5 @@
 package ca.jrvs.apps.trading;
 
-class HelloWorld {
+class Application {
 
-  // Your program begins with a call to main().
-  // Prints "Hello, World" to the terminal window.
-  public static void main(String args[]) {
-    System.out.println("Hello, World");
-  }
 }
